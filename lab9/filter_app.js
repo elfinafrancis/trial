fdabb5c5505d9ce0f9270711f2cf661b1c1ef31b
elfinafrancis <69503303+elfinafrancis@users.noreply.github.com>
2020-12-08 12:15:12 +0530

    lab9

--- lab9/filter_app.js
+++ lab9/filter_app.js
@@ -1,13 +1,49 @@
-angular.module("MyApp",[])
-.controller("MyCtrl",function($scope)
+angular.module('myapp',[])
+.controller('myctrl',function($scope)
 {
-    var employees=[
-        {name:'Rithul',dob:new Date("July 21,1990"),gender:"Male",salary:55000.778},
-        {name:'Indu',dob:new Date("July 20,1987"),gender:"Female",salary:65000.778},
-        {name:'Kevin',dob:new Date("November 19,1983"),gender:"Male",salary:55000.778},
-        {name:'Reena',dob:new Date("August 21,1990"),gender:"Male",salary:55000.778}
+    $scope.listCourses=[
+        {id:'C01',name:'Python',price:5000,hours:72},
+        {id:'C02',name:'Java',price:3455,hours:48},
+        {id:'C03',name:'C++',price:4205,hours:36},
+        {id:'C04',name:'C programming',price:2000,hours:24}
     ];
-    console.log(employees);
-    $scope.employees=employees;
-    $scope.rowlimit=4;
+    $scope.save=function()
+    {
+        var index=getindex($scope.id);
+        $scope.listCourses[index].name=$scope.name;
+        $scope.listCourses[index].price=$scope.price;
+        $scope.listCourses[index].hours=$scope.hours;
+    }
+    $scope.add=function()
+    {
+        $scope.listCourses.push({
+            id:$scope.id,name:$scope.name,price:$scope.price,hours:$scope.hours
+        });
+    }
+    function getindex(id)
+    {
+        for(var i=0;i<$scope.listCourses.length;i++)
+        if($scope.listCourses[i].id==id)
+        return i;
+        return -1;
+    }
+    $scope.edit=function(id)
+    {
+        var index=this.$index;
+        var course=$scope.listCourses[index];
+        $scope.id=course.id;
+        $scope.name=course.name;
+        $scope.price=course.price;
+        $scope.hours=course.hours;
+    }
+    $scope.delete=function()
+    {
+        var result=confirm('Are you sure?');
+        if(result==true)
+        {
+            var index=this.$index;
+            $scope.listCourses.splice(index,1);
+        }
+    };
 });
+
